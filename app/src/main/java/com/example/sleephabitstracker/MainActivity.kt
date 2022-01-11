0d3dcaa5dceda275ba2ddd4b11e5829cd5459746
Fryzu <bartek.fryz@gmail.com>
2022-01-11 20:07:54 +0100

    Adds broadcast receiver for the sleep events

--- app/src/main/java/com/example/sleephabitstracker/MainActivity.kt
+++ app/src/main/java/com/example/sleephabitstracker/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.sleephabitstracker
 
+import android.app.PendingIntent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sleephabitstracker.databinding.ActivityMainBinding
+import com.example.sleephabitstracker.receivers.SleepEventsReceiver
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sleepPendingIntent: PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sleepPendingIntent = SleepEventsReceiver.createSleepReceiverPendingIntent(applicationContext)
     }
 }
---
+++ app/src/main/java/com/example/sleephabitstracker/receivers/SleepEventsReceiver.kt
@@ -0,0 +1,68 @@
+package com.example.sleephabitstracker.receivers
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.example.sleephabitstracker.state.SleepHabitsTrackerDatabase
+import com.example.sleephabitstracker.state.SleepHabitsTrackerRepository
+import com.example.sleephabitstracker.state.datastore.SleepEventsSubscriptionStatus
+import com.example.sleephabitstracker.state.sleepevent.SleepEvent
+import com.google.android.gms.location.SleepClassifyEvent
+import com.google.android.gms.location.SleepSegmentEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
+/**
+ * Nasłuchuje na zdarzenia wykrycia snu i zapisuje je w bazie danych
+ */
+class SleepEventsReceiver : BroadcastReceiver() {
+
+    private val scope: CoroutineScope = MainScope()
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "onReceive(): $intent")
+
+        val sleepEventDAO = SleepHabitsTrackerDatabase.getInstance(context).sleepEventDAO()
+        val sleepEventsSubscriptionStatus = SleepEventsSubscriptionStatus(context)
+        val repository = SleepHabitsTrackerRepository(sleepEventDAO, sleepEventsSubscriptionStatus)
+
+
+        if (SleepSegmentEvent.hasEvents(intent)) {
+            val sleepSegmentEvents: List<SleepSegmentEvent> =
+                SleepSegmentEvent.extractEvents(intent)
+            Log.d(TAG, "SleepSegmentEvent List: $sleepSegmentEvents")
+            addSleepEventsToDatabase(repository, sleepSegmentEvents)
+        }
+    }
+
+    private fun addSleepEventsToDatabase(
+        repository: SleepHabitsTrackerRepository,
+        sleepSegmentEvents: List<SleepSegmentEvent>
+    ) {
+        if (sleepSegmentEvents.isNotEmpty()) {
+            scope.launch {
+                val convertedToEntityVersion: List<SleepEvent> =
+                    sleepSegmentEvents.map {
+                        SleepEvent.fromLocationEvent(it)
+                    }
+                repository.insertSleepEvents(convertedToEntityVersion)
+            }
+        }
+    }
+
+    companion object {
+        const val TAG = "SleepEventsReceiver"
+        fun createSleepReceiverPendingIntent(context: Context): PendingIntent {
+            val sleepIntent = Intent(context, SleepEventsReceiver::class.java)
+            return PendingIntent.getBroadcast(
+                context,
+                0,
+                sleepIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+            )
+        }
+    }
+}
