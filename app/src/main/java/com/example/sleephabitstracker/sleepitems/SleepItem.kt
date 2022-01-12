0c46bc20576b3c9765e556bd272235afbfb33ad2
Fryzu <bartek.fryz@gmail.com>
2022-01-12 14:36:16 +0100

    Displays parsed sleep time

--- app/src/main/java/com/example/sleephabitstracker/sleepitems/SleepItem.kt
+++ app/src/main/java/com/example/sleephabitstracker/sleepitems/SleepItem.kt
@@ -2,18 +2,17 @@ package com.example.sleephabitstracker.sleepitems
 
 import android.util.Log
 import java.time.Instant
+import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 data class SleepItem(val id: Long, val startTimestamp: Long, val endTimestamp: Long) {
     companion object {
-        fun getDateStrFromUnix(ms: Long): String {
+        fun getDateObjectFromUnix(ms: Long): LocalDateTime {
             Log.d("SleepItem", ms.toString())
-            var dateTime =  Instant.ofEpochSecond(ms / 1000)
+            return Instant.ofEpochSecond(ms / 1000)
                 .atZone(ZoneId.systemDefault())
                 .toLocalDateTime()
-            var formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
-            return formatter.format(dateTime);
         }
     }
 }
--- app/src/main/java/com/example/sleephabitstracker/sleepitems/SleepItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/sleephabitstracker/sleepitems/SleepItemRecyclerViewAdapter.kt
@@ -6,28 +6,40 @@ import android.view.ViewGroup
 import android.widget.TextView
 
 import com.example.sleephabitstracker.databinding.SleepItemBinding
+import java.time.Duration
+import java.time.format.DateTimeFormatter
 
 class SleepItemRecyclerViewAdapter(
         private val values: MutableList<SleepItem>)
     : RecyclerView.Adapter<SleepItemRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-    return ViewHolder(SleepItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-
+        return ViewHolder(SleepItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
+
+    private var hourFormatter = DateTimeFormatter.ofPattern("HH mm")
+    private var dateFormatter = DateTimeFormatter.ofPattern("dd LLL uuuu")
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.startDate.text = SleepItem.getDateStrFromUnix(item.startTimestamp)
-        holder.endDate.text = SleepItem.getDateStrFromUnix(item.endTimestamp)
+
+        val startDate = SleepItem.getDateObjectFromUnix(item.startTimestamp)
+        val endDate = SleepItem.getDateObjectFromUnix(item.endTimestamp)
+        val duration = Duration.between(startDate, endDate)
+
+        holder.startTime.text = "Start time: ${startDate.format(hourFormatter)}"
+        holder.endTime.text = "End time: ${endDate.format(hourFormatter)}"
+        holder.sleep_duration.text = "${duration.toHours()}h"
+        holder.startDate.text = startDate.format(dateFormatter)
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(binding: SleepItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val startDate: TextView = binding.startDate
-        val endDate: TextView = binding.endDate
+        val endTime: TextView = binding.endTime
+        val startTime: TextView = binding.startTime
+        val sleep_duration: TextView = binding.sleepDuration
 
         override fun toString(): String {
             return super.toString() + " '" + startDate.text + "'"
