bcaf899b9f65bf74cc4865ce7b9c738c9d1a3743
Fryzu <bartek.fryz@gmail.com>
2022-01-12 14:01:49 +0100

    Fixes date formatting

--- app/src/main/java/com/example/sleephabitstracker/sleepitems/SleepItem.kt
+++ app/src/main/java/com/example/sleephabitstracker/sleepitems/SleepItem.kt
@@ -1,15 +1,19 @@
 package com.example.sleephabitstracker.sleepitems
 
+import android.util.Log
 import java.time.Instant
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 data class SleepItem(val id: Long, val startTimestamp: Long, val endTimestamp: Long) {
     companion object {
         fun getDateStrFromUnix(ms: Long): String {
-            var dateTime =  Instant.ofEpochSecond(ms)
+            Log.d("SleepItem", ms.toString())
+            var dateTime =  Instant.ofEpochSecond(ms / 1000)
                 .atZone(ZoneId.systemDefault())
                 .toLocalDateTime()
-            return dateTime.toString()
+            var formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            return formatter.format(dateTime);
         }
     }
 }
